2ea269815d58d683a684e42ab457abd3c2049cd2
Hanakus0 <everydoghas12day@outlook.jp>
2024-04-16 08:00:20 +0900

    End 7.1

--- chapter7/sample/location/history_push.js
+++ chapter7/sample/location/history_push.js
@@ -3,3 +3,14 @@
 var count = 0;
 var result = document.getElementById('result');
 
+// [カウントアップ]ボタンをクリックしたときに履歴を追加
+document.getElementById('btn').addEventListener('click', function() {
+  result.textContent = ++count;
+  history.pushState(count, null, '/js/chap07/count', + count);
+});
+
+// [戻る]ボタンでページの状態を前に戻す
+// window.addEventListener('popstate', function(e) {
+//   count = e.state;
+//   result.textCount = count;
+// });
